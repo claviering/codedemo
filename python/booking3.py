c2f401552fea928b2c28342e049d316c132a0f9f
claviering <claviering@gmail.com>
2022-02-16 15:40:04 +0800

    3

--- python/booking3.py
+++ python/booking3.py
@@ -1,7 +1,9 @@
+from curses.ascii import NUL
 import random
 import time
 import requests
 from PIL import Image
+from PIL import UnidentifiedImageError
 from io import BytesIO
 from datetime import timedelta
 from datetime import date
@@ -42,6 +44,12 @@ def getVerify(session, num=1):
     except requests.exceptions.Timeout:
         print("%d 获取验证码超时, 重试中..." % (num))
         return getVerify(session, num+1)
+    except requests.exceptions.ConnectionError:
+        print("%d 获取验证码 ConnectionError, 重试中..." % (num))
+        return getVerify(session, num+1)
+    except UnidentifiedImageError:
+        print("%d 获取验证码 UnidentifiedImageError, 重试中..." % (num))
+        return getVerify(session, num+1)
 
 # ED9101314：3, RUQ5MTAxMzE0, OTIwNzc2ODU1ZjVjZmI0YzZhNzg2YzA3OWViNGFhYjM=
 # EJ3931121: 3, RUozOTMxMTIx, OGQwOTQzN2I3YzYzMzA0YTAzZTY3YmQ1MTJkNmRlNmE=
@@ -70,7 +78,7 @@ def login(session, code, num=1):
     print("登陆中...")
     try:
         response = session.post(host + '/user/login',
-                                headers=Headers, data=payload, timeout=3)
+                                headers=Headers, data=payload, timeout=10)
         status = response.json()['status']
         if status != 200:
             print("登陆失败, %s" % (response.text))
@@ -80,6 +88,9 @@ def login(session, code, num=1):
             return
     except requests.exceptions.Timeout:
         print("%d 登陆超时, 重试中..." % (num))
+        return login(session, code, num+1)
+    except requests.exceptions.ConnectionError:
+        print("%d ConnectionError, 重试中..." % (num))
         return login(session, code, num+1)
 
 
@@ -92,7 +103,6 @@ def isAfter0959AM():
 
 
 def getBookingUrl(session, num=1):
-    url = ''
     Headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.125 Safari/537.36',
         "Host": "hk.sz.gov.cn:8118",
@@ -106,25 +116,35 @@ def getBookingUrl(session, num=1):
     }
     now = time.localtime()
     print("线程 %d 请求数据时间: %d:%d:%d" % (num, now.tm_hour, now.tm_min, now.tm_sec))
-    response = session.post(
-        host + '/districtHousenumLog/getList', headers=Headers)
-    data = response.json()['data']
-    now = time.localtime()
-    print("线程 %d 返回数据时间: %d:%d:%d" % (num, now.tm_hour, now.tm_min, now.tm_sec))
-    for item in data:
-        print("预约日期: %s, 可预约数: %d, 总预约数: %d, sign: %s" %
-              (item['date'], item['count'], item['total'], item['sign']))
-        if item['date'] == endDay:
-            checkinDate = item['date']
-            timespan = item['timespan']
-            sign = item['sign']
-            url = host + "/passInfo/confirmOrder?checkinDate=" + \
-                checkinDate + "&t=" + str(timespan) + "&s=" + sign
-    return url
+    try:
+        response = session.post(
+            host + '/districtHousenumLog/getList', headers=Headers)
+        data = response.json()['data']
+        now = time.localtime()
+        print("线程 %d 返回数据时间: %d:%d:%d" % (num, now.tm_hour, now.tm_min, now.tm_sec))
+        for item in data:
+            print("预约日期: %s, 可预约数: %d, 总预约数: %d, sign: %s" %
+                (item['date'], item['count'], item['total'], item['sign']))
+            if item['date'] == endDay:
+                checkinDate = item['date']
+                timespan = item['timespan']
+                sign = item['sign']
+                return checkinDate, str(timespan), sign
+    except requests.exceptions.Timeout:
+        print("%d 登陆超时, 重试中..." % (num))
+        return getBookingUrl(session, num+1)
+    except requests.exceptions.ConnectionError:
+        print("%d ConnectionError, 重试中..." % (num))
+        return getBookingUrl(session, num+1)
+    except requests.exceptions.JSONDecodeError:
+        print("%d JSONDecodeError, 重试中..." % (num))
+        return getBookingUrl(session, num+1)
 
 
-def booking(session, num=1):
-    url = getBookingUrl(session, num)
+def booking(session, num, code=None):
+    checkinDate, timespan, sign = getBookingUrl(session, num)
+    if not code:
+        code = getVerify(session, num)
     Headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.125 Safari/537.36',
         "Host": "hk.sz.gov.cn:8118",
@@ -134,18 +154,26 @@ def booking(session, num=1):
         "Sec-Fetch-Site": "same-origin",
         "Connection": "keep-alive"
     }
+    url = host + "/passInfo/submitReservation"
+    payload = {
+        "checkinDate": checkinDate,
+        "checkCode": code,
+        "houseType": "1",
+        "t": timespan,
+        "s": sign,
+    }
     print('线程 %d 拼命抢号中...' % (num))
-    response = session.get(url, headers=Headers, verify=False)
-    print(response.text)
-    if '未到开放时间' in response.text:
-        print("未到开放时间, 抢号中...")
-    elif '沒有可預約的數據' in response.text:
-        print("线程 %d 沒有可預約的數據, 抢号结束!" % (num))
-    else:
-        # 没有代码，先保存再分析一下
-        with open("booking" + str(random.random()) + ".html", "w", encoding="utf-8") as f:
-            f.write(response.text)
-    return
+    try:
+        response = session.post(url, headers=Headers, data=payload, verify=False)
+        print("线程 %d 结果: %s" % (num, response.text))
+        return
+    except requests.exceptions.ConnectionError:
+        print("%d ConnectionError, 重试中..." % (num))
+        return booking(session, num+1, code)
+
+def test(a, b):
+    code = input("请输入验证码: ")
+    print(code)
 
 def main():
     session = requests.Session()
