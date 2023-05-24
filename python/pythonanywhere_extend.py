7d78ec2dacaaf13077f8773897f3b7d6282c7453
claviering <claviering@gmail.com>
2023-05-24 16:24:14 +0800

    pythonanywhere_extend 添加登录

--- python/pythonanywhere_extend.py
+++ python/pythonanywhere_extend.py
@@ -1,23 +1,36 @@
 #!/usr/local/bin/python
 # -*- coding: utf-8 -*-
 # pythonanywhere 签到延期脚本
+# 使用 python3 pythonanywhere_extend.py <username> <password>
 
 import requests
 import re
+import sys
 
-cookie = 'web_app_tab_type=%23tab_id_hnaruto_pythonanywhere_com; cookie_warning_seen=True; __stripe_mid=9e0449b1-34e5-4833-add3-9aaa52fadf15d0034b; csrftoken=MPJ8zjUkxb6F3gHVKmlubjDnVQP8qkR0B4YlF6eUOFZGF3NXdjNuEcNedadKH9NI; sessionid=w6yjqwnuhnxboa6k4wscq3m0wy36g4bj'
+login_url = "https://www.pythonanywhere.com/login/"
+
+webapps_url = "https://www.pythonanywhere.com/user/hnaruto/webapps/"
+
+extend_url = "https://www.pythonanywhere.com/user/hnaruto/webapps/hnaruto.pythonanywhere.com/extend"
 
 
-def main():
+def get_cookie(response):
+    set_cookie = response.headers.get('Set-Cookie')
+    return set_cookie
+
+
+def get_headers(cookie, referer):
     headers = {
         "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
         "Accept-Encoding": "gzip, deflate, br",
         "Accept-Language": "en-US,en;q=0.9,zh-CN;q=0.8,zh;q=0.7",
         "Cache-Control": "max-age=0",
         "Connection": "keep-alive",
-        "Cookie": cookie,
+        "Content-Type": "application/x-www-form-urlencoded",
         "Host": "www.pythonanywhere.com",
-        "Referer": "https://www.pythonanywhere.com/user/hnaruto/webapps/",
+        "Origin": "https://www.pythonanywhere.com",
+        "Cookie": cookie,
+        "Referer": referer,
         "sec-ch-ua-mobile": "?0",
         "sec-ch-ua-platform": "macOS",
         "Sec-Fetch-Dest": "document",
@@ -27,23 +40,53 @@ def main():
         "Upgrade-Insecure-Requests": "1",
         "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36"
     }
+    return headers
 
-    response = requests.get(
-        "https://www.pythonanywhere.com/user/hnaruto/webapps/", headers=headers, data=None)
 
+def getToken(response):
     pattern = '<input\s+type="hidden"\s+name="csrfmiddlewaretoken"\s+value="([\w]+)">'
     result = re.search(pattern, response.text)
     csrfmiddlewaretoken = ''
     if result:
-        print(result.group(1))
         csrfmiddlewaretoken = result.group(1)
+    return csrfmiddlewaretoken
 
+
+def login(sss, username, password):
+    headers = get_headers(None, login_url)
+    response = sss.get(login_url, headers=headers)
+    cookie = get_cookie(response)
+    headers['Cookie'] = cookie
+    csrfmiddlewaretoken = getToken(response)
+    data = {
+        'auth-username': username,
+        'auth-password': password,
+        'login_view-current_step': 'auth',
+        "csrfmiddlewaretoken": csrfmiddlewaretoken
+    }
+    r = sss.post(login_url, data=data, headers=headers)
+    return r
+
+
+def extend(sss):
+    headers = get_headers(None, webapps_url)
+    response = sss.get(webapps_url, headers=headers)
+    cookie = get_cookie(response)
+    headers['Cookie'] = cookie
+    csrfmiddlewaretoken = getToken(response)
     data = {
         'csrfmiddlewaretoken': csrfmiddlewaretoken
     }
-    response = requests.post(
-        "https://www.pythonanywhere.com/user/hnaruto/webapps/hnaruto.pythonanywhere.com/extend", headers=headers, data=data)
+    response = sss.post(extend_url, data=data, headers=headers)
     print("pythonanywhere: " + str(response.status_code))
+
+
+def main():
+    username = sys.argv[1]
+    password = sys.argv[2]
+    sss = requests.Session()
+    login(sss, username, password)
+    extend(sss)
 
 
 if __name__ == '__main__':
